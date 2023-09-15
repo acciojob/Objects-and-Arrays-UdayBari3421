46dc347dc8757e6fd524a232fc46d4bc33fe3d99
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-15 10:50:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const team1 = players.slice();
 // Create a shallow copy of the person object
 const cap1 = Object.assign({}, person);
 
-// Now you have the desired variables: team, team1, and cap1
-console.log("team:", team);
-console.log("team1:", team1);
-console.log("cap1:", cap1);
+// Attach the variables to the window object
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
